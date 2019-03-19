df3afcd5cf851fb9268e07970df3a4847626bd77
Shams-Ibne-Noor <40539713+shamsrupom94@users.noreply.github.com>
2019-03-19 20:39:24 +0600

    new update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,34 +5,35 @@ export default function About() {
     <div className="container">
       <h2 className="display-4 mb-3">
         About <span className="text-primary">React </span> Contact Manager
-        (Context API version)
       </h2>
       <p className="lead">
         Contact Manager Build with REACTJS , Context API/Redux (there's actually
-        two seperate versions of this project). We have also added react router
-        to browse through the app.{" "}
-        <span className="text-danger">
-          THIS VESION IS BUILD WITH Context API.
-        </span>
+        two seperate versions of this project).
+        <br />
+        <a
+          href="https://github.com/shamsrupom94/contact-manager-react-app-context"
+          target="_blank"
+          className="text-danger"
+          style={{ cursor: "pointer" }}
+        >
+          Github page for context API Version
+        </a>
+        <br />
+        <a
+          href="https://github.com/shamsrupom94/contact-manager-react-app-redux"
+          target="_blank"
+          className="text-danger"
+          style={{ cursor: "pointer" }}
+        >
+          Github page for Redux Version
+        </a>
       </p>
       <p className="lead text-danger">
         In this project we have not used any database. Here, we used JSON place
         holder as mock API
       </p>
-      <p className="lead">
-        {" "}
-        The Dependencies that are added to this version are{" "}
-      </p>
-      <ul>
-        <li>bootstrap (for styling)</li>
-        <li>
-          classnames (for adding multiple classnames and conditional classnames
-          in TextGroup.js file)
-        </li>
-        <li>uuid (for random id genarator)</li>
-        <li>react-router-dom (for router) </li>
-      </ul>
-      <p className="text-secondary"> Version 1.0.1</p>
+
+      <p className="text-secondary"> Version 1.0.2</p>
     </div>
   );
 }
